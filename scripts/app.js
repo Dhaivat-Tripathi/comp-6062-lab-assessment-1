dfe2cab21f5c77633f8dfccf8e2e19ba000de1a1
Dhaivat-Tripathi <tripathidhaivat@gmail.com>
2025-02-13 19:20:10 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,12 @@ const studentFullName = "Dhaivat Tripathi";
 const studentNumber = "1162646";
 
 // log suing templete literals
-console.log(`${studentFullName} ${studentNumber}`);
+console.log(`${studentFullName} - ${studentNumber}`);
 
 const headerContent = document.querySelector('h1');
 
 // student detail added to h1 in html
-headerContent.innerHTML = `${studentFullName} ${studentNumber}`;
+headerContent.innerHTML = `${studentFullName} - ${studentNumber}`;
 
 // adding class headingPrimary to the h1 in html
 headerContent.classList.add("headingPrimary");
